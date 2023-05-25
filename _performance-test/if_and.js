0f3922b13779e63fa7ae02f5a79297d9d3effc84
GyuCheol Lee <charles.lee@innorules.com>
2023-05-25 12:22:18 +0900

    add if-and benchmarks

---
+++ _performance-test/if_and.js
@@ -0,0 +1,37 @@
+const bl = true;
+const TEST_LENGTH = 500000000;
+
+function foo() {
+  let id = 0;
+
+  return id++;
+  // nothing
+}
+
+function benchmark_for_if() {
+  for (let index = 0; index < TEST_LENGTH; index++) {
+    if (bl) foo(); 
+  }
+}
+
+function benchmark_for_and() {
+  for (let index = 0; index < TEST_LENGTH; index++) {
+    bl && foo(); 
+  }
+}
+
+function printDuration(label, fn, loop) {
+  console.time(label)
+
+  for (let index = 0; index < loop; index++)
+    fn();
+
+  console.timeLog(label)
+}
+
+// printDuration("and", benchmark_for_and, 10)
+// printDuration("if", benchmark_for_if, 10)
+
+/**
+ * 결론: if나 and나 똑같다. 가독성과 관련된 문제이다.
+ */
