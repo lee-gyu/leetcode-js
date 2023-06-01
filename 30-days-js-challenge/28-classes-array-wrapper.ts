71aaa37ca9b0e87492e2522b275188947535e7ae
leegyu <leegyu4dev@gmail.com>
2023-06-01 16:01:10 +0900

    add #28

---
+++ 30-days-js-challenge/28-classes-array-wrapper.ts
@@ -0,0 +1,21 @@
+class ArrayWrapper {
+  nums: number[];
+
+  constructor(nums: number[]) {
+    this.nums = nums;
+  }
+
+  valueOf() {
+    return this.nums.reduce((acc, num) => acc + num, 0);
+  }
+
+  toString() {
+    return `[${this.nums.join(',')}]`;
+  }
+}
+
+/**
+ * prototype의 valueOf, toString 구현을 묻는 문제.
+ * 객체의 연산자 오버로딩을 구현하는 문제이다.
+ * number의 + 연산이나 string concat 연산의 경우 호출되는 메서드
+ */
