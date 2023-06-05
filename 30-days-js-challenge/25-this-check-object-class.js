c66265b4180ac859b45e5f3019885e9a2347164e
lee-gyu <leegyu4dev@gmail.com>
2023-06-05 09:45:51 +0900

    edit comments

--- 30-days-js-challenge/25-this-check-object-class.js
+++ 30-days-js-challenge/25-this-check-object-class.js
@@ -27,4 +27,6 @@ console.log(checkIfInstanceOf(null, null)); // false
  * __proto__보다는 Object.getPrototypeOf()를 사용하는 것이 좋다. (성능, 메모리면에서 훨씬 효율적이다.)
  * constructor를 이용하여 해결하는 솔루션도 있음
  * 
+ * 인스턴스를 비교할 class의 prototype과 현재 오브젝트의 프로토타입 체인을 비교
+ * 
  */
